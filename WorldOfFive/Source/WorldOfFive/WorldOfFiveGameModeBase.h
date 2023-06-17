480ebaa4caca336b1f5cf71edc12bcd9bf26e214
D4vvon <48454979+D4vvon@users.noreply.github.com>
2023-06-18 00:48:19 +0300

    Create project with dev map

---
+++ WorldOfFive/Source/WorldOfFive/WorldOfFiveGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WorldOfFiveGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WORLDOFFIVE_API AWorldOfFiveGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
